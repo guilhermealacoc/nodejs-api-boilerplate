08fbd935863c08c62e6e64bc4608555fd191cec7
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2020-12-01 19:33:55 -0300

    feat: rotas, server e module

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,9 @@
 const { Router }= require('express');
+
+const routes = new Router();
+
+routes.get('/', (req, res) =>{
+    res.send("Guilherme de Alacoc");
+})
+
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-
+const app = require('./app');
 
 app.listen(8080, () =>{
     console.log("Servidor Iniciado na porta 8080");
